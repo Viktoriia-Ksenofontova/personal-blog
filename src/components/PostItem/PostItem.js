c730926baeb1ef38c71e7dd08a26183b2da4e50c
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2022-01-06 13:34:11 +0200

    add component Text

--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useFela } from "react-fela";
+import Text from "../Text/Text";
 
 const linkRule = () => ({
   display: 'block',
@@ -18,23 +19,30 @@ const linkRule = () => ({
   },
 });
 
+const postStyle = {
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  display: '-webkit-box',
+  '-webkitLineClamp': '3',
+  '-webkitBoxOrient': 'vertical'
+}
+
 export default function PostItem (props) {
   const { id, title, body } = props;
-   const { css } = useFela();
+  const { css } = useFela();
   return (
-  <>
-    <h3>{title}</h3>
-      <p className={css({
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        display: '-webkit-box',
-        '-webkitLineClamp': '3',
-        '-webkitBoxOrient': 'vertical'
-      })}>
+    <>
+      <Text as="h3" variant="heading3">
+        {title}
+      </Text>
+      <Text as="p" styles={postStyle }>
         {body}
-      </p>
-      <Link to={`posts/:${id}`} className={css(linkRule)}> Read more</Link>
-  </>
+      </Text>
+      
+      <Link to={`posts/:${id}`} className={css(linkRule)}>
+        Read more
+      </Link>
+    </>
   )  
 };
 
---
+++ src/components/Text/Text.theme.js
@@ -0,0 +1,28 @@
+const heading1 = {
+  fontSize: 32,
+  fontWeight: 700,
+};
+const heading2 = {
+  fontSize: 24,
+  fontWeight: 700,
+};
+const heading3 = {
+  fontSize: 18,
+  fontWeight: 700
+};
+const primary = {
+  fontSize: 16,
+  fontWeight: 400,
+};
+const small = {
+  fontSize: 12,
+  fontWeight: 400
+}
+
+export default {
+  heading1,
+  heading2,
+  heading3,
+  primary,
+  small
+};
