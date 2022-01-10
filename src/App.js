711f29866f754388cfcf7fa3191f319c8f19d8e4
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2022-01-10 21:15:50 +0200

    modification store - add newPostStore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Suspense, useMemo, useState } from "react";
+import React, { Suspense, useMemo, useState} from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
 import { createRenderer } from 'fela';
 import { RendererProvider } from 'react-fela';
 import routes from "./routing/routes";
 import mainPostsStore from "./store/PostsStore";
+import newPostsStore from './store/newPostStore';
+
 import { Footer, Header, Main } from "./components";
 import ThemeContext from "./context/ThemeContext";
 
@@ -13,6 +15,7 @@ const PostPage = React.lazy(() => import("./pages/PostPage"));
 
 const renderer = createRenderer();
 
+  
 function App() {
   const [theme, setTheme] = useState("light");
   const value = useMemo(()=>({theme, setTheme }),[theme]);
@@ -25,9 +28,9 @@ function App() {
       <Main>
         <Suspense fallback={<p>Loading...</p>}>
           <Routes>
-            <Route exact path={routes.home} element={<LatestPosts store={mainPostsStore}/>} />
+            <Route exact path={routes.home} element={<LatestPosts store={newPostsStore}/>} />
             <Route path={routes.createPost} element={<CreatePost store={mainPostsStore}/>} />
-            <Route path={routes.post} element={<PostPage/>}/>
+            <Route path={routes.post} element={<PostPage store={mainPostsStore}/>}/>
             <Route path="*" element={<Navigate to={routes.page404} />} />
           </Routes>
         </Suspense>
--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,47 +1,78 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { observer } from "mobx-react-lite";
 import { useFela } from 'react-fela';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate  } from "react-router-dom";
 import { fetchPosts, createComment } from "../../services/postsApi";
 import { Container, Form, CommentItem, List, Button, Text } from '../../components';
 import ThemeContext from "../../context/ThemeContext";
+import DeleteIcon from "../../assets/images/deleteIcon.svg";
 import palette from '../../assets/colors';
 
-export default function PostPage() {
+const PostPage = observer(({ store }) => {
+  const navigate = useNavigate();
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [comments, setComments] = useState([]);
   const [status, setStatus] = useState("");
   const [newComment, setNewComment] = useState("");
   const { postId } = useParams();
+  const correctPostId = Number(postId.slice(1));
+  
   const { theme } = useContext(ThemeContext);
-
   const { css } = useFela();
 
+  
+
   useEffect(() => {
-    fetchPosts(`/posts/${postId.slice(1)}?_embed=comments`).then(res => {
+    fetchPosts(`/posts/${correctPostId}?_embed=comments`).then(res => {
       setTitle(res.posts.title);
       setBody(res.posts.body);
       setComments(res.posts.comments);
       setStatus(res.status)
     })
-  }, [postId]);
+  }, [correctPostId]);
   
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    createComment(postId, newComment).then(res => {
+    createComment(correctPostId, newComment).then(res => {
       setComments([...comments, res.comment]);
       setNewComment("")
     })
+  };
+
+ 
+  const handleClick = e => {
+    e.preventDefault();
+    store.removePost(correctPostId).then((res) => {
+      setStatus(res);
+      navigate(`/`);
+    });  
   }
+
+  const buttonDeleteRule = () => ({
+    width: '30px',
+    marginLeft: '50px',
+    height: '30px',
+    border: `1px solid ${palette[theme].accent}`,
+    borderRadius: '50%',
+    transition: 'all 0.4s linear',
+    ':hover': { backgroundColor: palette[theme].accent },
+    ':focus': { backgroundColor: palette[theme].accent }
+  })
 
   return (
     <Container>
       {status === "pending" && <div>Loading...</div>}
+      <div className={css({ display: 'flex', justifyContent:'center'})}>
+        <Text as='h2' styles={{ textAlign: "center" }} variant='heading2'>{title}</Text>
+        <button type="button" onClick={handleClick} 
+          className={css(buttonDeleteRule)}>
+         <img src={DeleteIcon} alt="delete"/>
+        </button>
+      </div>
       
-      <Text as='h2' styles={{textAlign:"center"}} variant='heading2'>{title}</Text>
       <Text as='p'>{body}</Text>
-
       <Text as='h3' variant='heading3'>Comments:</Text>
       
       {comments?.length === 0 ?
@@ -87,5 +118,6 @@ export default function PostPage() {
       </Form>
     </Container>
   )
-}
+})
 
+export default PostPage;
