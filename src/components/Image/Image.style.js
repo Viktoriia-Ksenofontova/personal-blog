8b141d0ba9b27ffe8f01420aa1679b6a64f1d4f1
Viktoriia Ksenofontova <66468364+Viktoriia-Ksenofontova@users.noreply.github.com>
2022-01-12 17:26:16 +0200

    feature/CPB-22 add component image

---
+++ src/components/Image/Image.style.js
@@ -0,0 +1,64 @@
+const themeIcon = {
+    iconSize:{
+        large: 44,
+        small: 30,
+        default: 20,
+    },
+    color:{
+        primary: '#111',
+        accent:'#9ACD32'
+    }  
+};
+
+const getIconSize=(theme, size)=>{
+    switch(size){
+        case 'sizeLarge':
+            return theme.iconSize.large;
+           
+        case 'sizeSmall':
+            return theme.iconSize.small;
+           
+        default:
+         return theme.iconSize.default;
+    }
+};
+
+const getIconColor=(theme, colorName)=>{
+    switch(colorName){
+        case 'primary':
+            return theme.color.primary;
+
+        case 'accent':
+            return theme.color.accent;
+
+        default:
+            return theme.color.primary;
+    }
+}
+  
+export default function style(size, iconColor, styles) {
+    let height;
+    let width;
+    let fill;
+
+    if(iconColor){ 
+        fill=getIconColor(themeIcon, iconColor);
+    } else if(styles?.fill){
+        fill=styles.fill;
+    }
+
+    if(size){
+        height= getIconSize(themeIcon, size);
+        width= getIconSize(themeIcon, size); 
+        
+    } else if(styles?.height || styles?.width){
+        height= styles.height || getIconSize(themeIcon, 'default');
+        width= styles.width || getIconSize(themeIcon, 'default');     
+        
+    } else {  
+        height= getIconSize(themeIcon, 'default');
+        width= getIconSize(themeIcon, 'default');
+       
+    }
+    return({height, width, fill})
+}
