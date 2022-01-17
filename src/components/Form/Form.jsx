ec5c772f19e02718d0fe847c028dcdaec0997a2c
Viktoriia Ksenofontova <66468364+Viktoriia-Ksenofontova@users.noreply.github.com>
2022-01-17 17:32:04 +0200

    bugFix/CPB move styles from Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { useFela } from 'react-fela';
 import PropTypes from 'prop-types';
+import makeFormStyle from './Form.style';
 
 export default function Form({ handleSubmit, children }){
   const { css } = useFela();
  
   return (   
-     <form onSubmit={handleSubmit}
-        className={css({
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent:'center',
-          alignItems:'center'
-      })}>
+    <form onSubmit={handleSubmit} className={css(makeFormStyle)}>
       {children}
-      </form>
+    </form>
   )
 }
 
