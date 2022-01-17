414b8da4bb88fadc6be03403a5d096d4f95e27b4
Viktoriia Ksenofontova <66468364+Viktoriia-Ksenofontova@users.noreply.github.com>
2022-01-17 10:12:17 +0200

    change position of loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { useFela } from 'react-fela';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import { Triangle } from  'react-loader-spinner'
+import { Triangle } from  'react-loader-spinner';
+import makeStyles from './Loader.style';
 
 function Loader(){
-   return (
-     <Triangle
-        arialLabel='loading'
-        color="#9ACD32"
-        height={80}
-        width={80}
-        timeout={3000}
-    />
+    const { css } = useFela();
+    return (
+        <div className={css(makeStyles)}>
+            <Triangle
+                arialLabel='loading'
+                color="#9ACD32"
+                height={80}
+                width={80}
+                timeout={3000}
+            />
+        </div>
   );
   }
 
